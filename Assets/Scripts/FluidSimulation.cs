1ab7e0910da311ea8a93bbad80dbf05bbb37e6f2
Adam Joyce <adamjoyce@outlook.com>
2016-05-12 12:09:17 +0100

    Setting up basic materials

---
+++ Assets/Scripts/FluidSimulation.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FluidSimulation : MonoBehaviour {
+
+    public Material advectMaterial;
+    public Material jacobiMaterial;
+    public Material divergenceMaterial;
+    public Material gradientMaterial;
+    public Material boundaryMaterial;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
